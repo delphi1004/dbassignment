901828758145773636bbd329ac2347a04a8e4f10
delphi1004 <delphi1004@hanmail.net>
2021-02-13 20:13:21 +0900

    port changed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const analyserRouter = require('./controllers/analyser')
+const healthRouter = require('./controllers/health')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -14,6 +15,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/analyse', analyserRouter)
+app.use('/health', healthRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 console.log(`the system running on ${process.env.NODE_ENV} mode`)
 
 module.exports = {
